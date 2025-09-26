c3f4048af3d22014f3b32f822441a6b46ad92fce
cabasa2425 <carlosbarretosantana@gmail.com>
2025-09-26 18:18:17 +0200

    First commit

---
+++ src/main/java/com/Kario/KarioApplication.java
@@ -0,0 +1,13 @@
+package com.Kario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Kario/KarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
